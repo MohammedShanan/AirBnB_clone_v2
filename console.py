c89cf8adea29d9eca0014d34fa13649c9680959c
mohammedshanan <mohammedshana12@gmail.com>
2024-07-26 02:08:45 +0300

    updated do create

--- console.py
+++ console.py
@@ -10,6 +10,7 @@ from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
+import re
 
 
 class HBNBCommand(cmd.Cmd):
@@ -73,7 +74,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -118,10 +119,22 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        args = args.split()
+        class_name = args[0]
+        if class_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
+        match = re.findall(r"(\w+=\s?.+)", " ".join(args[1:]))
+        if match:
+            match = match[0].replace("\"", "'")
+            match = match.split()
+            for i,v in enumerate(match):
+                match[i] = v.replace("=", ":")
+            print(match)
+            _dict = self.__create_dict(match)
+            new_instance = HBNBCommand.classes[args[0]](**_dict)
+        else:
+            new_instance = HBNBCommand.classes[args[0]]()
         storage.save()
         print(new_instance.id)
         storage.save()
@@ -272,7 +285,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +293,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
@@ -320,5 +333,21 @@ class HBNBCommand(cmd.Cmd):
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
 
+    def __parse_nums(self, string):
+        string = string.strip() 
+        if "'" in string:
+            return string.replace("'", "")
+        if string.isdigit() or (string.startswith("-") and "." not in string):
+            return int(string)
+        elif string.replace(".", "").isnumeric() or string.startswith("-"):
+            return float(string)
+        return string
+
+    def __create_dict(self, args):
+        new_dict = {}
+        for match in args:
+            k, v = map(self.__parse_nums, match.split(":"))
+            new_dict[k] = v.replace("_", " ") if isinstance(v, str) else v
+        return new_dict
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
+
 from datetime import datetime
 
 
@@ -8,19 +9,15 @@ class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
+        from models import storage
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            self.__set_attr(kwargs)
+            storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -42,3 +39,23 @@ class BaseModel:
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
+    def __set_attr(self, attr_dict):
+        """
+            private: converts attr_dict values to python class attributes
+        """
+        if 'id' not in attr_dict:
+            attr_dict['id'] = str(uuid.uuid4())
+        if 'created_at' not in attr_dict:
+            attr_dict['created_at'] = datetime.utcnow()
+        elif not isinstance(attr_dict['created_at'], datetime):
+            attr_dict['created_at'] = datetime.fromisoformat(
+                attr_dict['created_at'])
+        if 'updated_at' not in attr_dict:
+            attr_dict['updated_at'] = datetime.utcnow()
+        elif not isinstance(attr_dict['updated_at'], datetime):
+            attr_dict['updated_at'] = datetime.fromisoformat(
+                attr_dict['updated_at'])
+        if '__class__' in attr_dict.keys():
+            attr_dict.pop('__class__')
+        for attr, value in attr_dict.items():
+            setattr(self, attr, value)
