74dab185a84491c4926ea49df67b7538e9fa2ccf
mohammedshanan <mohammedshana12@gmail.com>
2024-07-27 19:50:19 +0300

    added DBStorage - States and Cities

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 """This module defines a New engine DBStorage"""
 
-
+from models.base_model import Base
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 from models.base_model import Base
 from os import getenv
 
@@ -25,3 +31,33 @@ class DBStorage:
         )
         if getenv("HBNB_ENV") == "test":
             Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        objs = []
+        classes = [State, City, User, Place, Review, Amenity]
+        if cls:
+            objs = self.__session.query(cls).all()
+        else:
+            for cls in classes:
+                objs.extend(self.__session.query(cls).all())
+        return {type(obj).__name__ + "." + obj.id: obj for obj in objs}
+
+    def new(self, obj):
+        """add the object to the current database session"""
+        self.__session.add(obj)
+
+    def save(self):
+        """commit all changes of the current database session"""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """delete from the current database session obj if not None"""
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """create all tables in the database and create the current database session"""
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
