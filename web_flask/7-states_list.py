f059b26df81f910bf678566548682d51f56e03a4
mohammedshanan <mohammedshana12@gmail.com>
2024-07-28 21:11:17 +0300

    added state_list

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Start web application with two routings
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def state_list():
+    """Render template with states"""
+    all_states = storage.all(State)
+    states = {}
+    for state in all_states.values():
+        states[state.id] = state.name
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def app_teardown(args=None):
+    """Clean-up session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
