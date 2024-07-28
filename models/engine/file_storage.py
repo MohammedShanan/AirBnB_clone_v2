c645f9da458540a76102f811239ee85fa2b58466
mohammedshanan <mohammedshana12@gmail.com>
2024-07-28 03:14:38 +0300

    updated amenity and place

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,6 +8,7 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
+
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
 
