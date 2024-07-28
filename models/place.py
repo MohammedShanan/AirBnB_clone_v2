0c2034962766532d09b3cc9a2055a3f4b435efce
mohammedshanan <mohammedshana12@gmail.com>
2024-07-28 03:03:17 +0300

    updated amenity and place

--- models/place.py
+++ models/place.py
@@ -2,9 +2,29 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from models.review import Review
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from models.amenity import Amenity
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from os import getenv
+
+place_amenity = Table(
+    "place_amenity",
+    Base.metadata,
+    Column(
+        "place_id",
+        String(60),
+        ForeignKey("places.id"),
+        nullable=False,
+        primary_key=True,
+    ),
+    Column(
+        "amenity_id",
+        String(60),
+        ForeignKey("amenities.id"),
+        nullable=False,
+        primary_key=True,
+    ),
+)
 
 
 class Place(BaseModel, Base):
@@ -23,6 +43,9 @@ class Place(BaseModel, Base):
     longitude = Column(Float, nullable=True)
     amenity_ids = []
     reviews = relationship("Review", cascade="all", backref="place")
+    amenities = relationship(
+        "Amenity", secondary=place_amenity, backref="place_amenity", viewonly=False
+    )
     if getenv("HBNB_TYPE_STORAGE") != "db":
 
         @property
@@ -35,3 +58,20 @@ class Place(BaseModel, Base):
                 if self.id == review.place_id:
                     place_reviews.append(review)
             return place_reviews
+
+        @property
+        def amenities(self):
+            from models import storage
+
+            amenities_list = []
+            all_amenities = storage.all()
+            for key, amenity in all_amenities.items():
+                if key in self.amenity_ids:
+                    amenities_list.append(amenity)
+            return amenities_list
+
+        @amenities.setter
+        def amenities(self, obj=None):
+            if type(obj).__name__ == "Amenity":
+                key = "Amenity" + "." + obj.id
+                self.amenity_ids.append(key)
